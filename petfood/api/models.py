61286503aec1926cd9d70ceaee27c1733bc28310
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2024-12-26 12:46:46 +0530

    registeraion

--- petfood/api/models.py
+++ petfood/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_deleted=models.BooleanField(default=False)
+    email=models.EmailField(unique=True)
+    def createsuperuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
