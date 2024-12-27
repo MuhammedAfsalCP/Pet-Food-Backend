4fe62f55d4f76dc2ad8f62e26286a25e1e84608e
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2024-12-27 16:39:44 +0530

    register serializer

--- petfood/api/urls.py
+++ petfood/api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import ProductDetails
+from .views import ProductDetails,UserDetails
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -10,7 +10,7 @@ router=DefaultRouter()
 router.register('productdetails',ProductDetails)
 
 urlpatterns = [
-    
+    path('register/',UserDetails.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
