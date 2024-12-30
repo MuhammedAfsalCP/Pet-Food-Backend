0b61456be9c5bc0ece98f1f53293d0d4f7a69b7f
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2024-12-30 10:39:19 +0530

    convert apps

---
+++ petfood/users/urls.py
@@ -0,0 +1,20 @@
+
+from django.urls import path
+from .views import UserDetails
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/',UserDetails.as_view()),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+]
+
+
+
+
