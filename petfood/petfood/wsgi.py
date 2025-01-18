91eac55ec9a337093a7f497e8098c4fb69b68d69
MuhammedAfsalCp <muhammedafsalcp8@gmail.com>
2025-01-18 16:03:10 +0530

    last

--- petfood/petfood/wsgi.py
+++ petfood/petfood/wsgi.py
@@ -17,8 +17,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Ensure the correct settings module path is provided
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petfood.petfood.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petfood.settings')
 
 application = get_wsgi_application()
 
